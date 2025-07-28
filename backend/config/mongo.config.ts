171400e50e69917bf5eb83d5f3463f3d20efee01
arunrajput <arun@bastionex.net>
2025-07-28 12:10:19 +0530

    setup of decentralized db

--- backend/config/mongo.config.ts
+++ backend/config/mongo.config.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const MONGO_URI = process.env.MONGO_URI;
+console.log(MONGO_URI,'this is mongo url====>')
 if (!MONGO_URI) {
   throw new Error("Please provide MONGO_URI in the environment variables");
 }
